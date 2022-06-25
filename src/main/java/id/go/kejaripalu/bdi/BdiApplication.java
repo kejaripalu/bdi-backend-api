badbb401c7e466e53a0fd756303c1b5070b7583c
Ucup TopekoX <ucup.topekox@gmail.com>
2022-06-25 23:55:10 +0800

    init commit

---
+++ src/main/java/id/go/kejaripalu/bdi/BdiApplication.java
@@ -0,0 +1,13 @@
+package id.go.kejaripalu.bdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdiApplication.class, args);
+	}
+
+}
