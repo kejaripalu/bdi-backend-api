4a9ff1fe1f7d8ee15e3d916eceb069f1f2dd344f
Ucup TopekoX <ucup.topekox@gmail.com>
2022-07-25 09:23:29 +0800

    Set Cross Origin

--- src/main/java/id/go/kejaripalu/bdi/controller/RegisterSuratMasukController.java
+++ src/main/java/id/go/kejaripalu/bdi/controller/RegisterSuratMasukController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1")
+@CrossOrigin("http://localhost:4200")
 public class RegisterSuratMasukController {
 
 	private RegisterSuratMasukService suratMasukService;
