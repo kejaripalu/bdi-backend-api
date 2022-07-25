01b5b6030460d77034841df7ba7eed9716ed8ca7
Ucup Topekox <ucup.topekox@gmail.com>
2022-07-25 22:08:58 +0800

    add Application properties prefix

---
+++ src/main/java/id/go/kejaripalu/bdi/config/ApplicationProperties.java
@@ -0,0 +1,16 @@
+package id.go.kejaripalu.bdi.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Configuration
+@ConfigurationProperties(prefix = "app")
+@Data
+public class ApplicationProperties {
+	
+	private String originUrl;
+	private String apiUrl;
+
+}
