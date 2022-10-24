a18d01660c2734ee451921df156d926e4c2ac6e5
Ucup Topekox <ucup.topekox@gmail.com>
2022-10-24 09:37:23 +0800

    add register arsip

---
+++ src/main/java/id/go/kejaripalu/bdi/domain/RegisterArsip.java
@@ -0,0 +1,65 @@
+package id.go.kejaripalu.bdi.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import id.go.kejaripalu.bdi.domain.util.BaseUUIDEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "register_arsip")
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterArsip extends BaseUUIDEntity {
+
+	@Column(name = "tanggal_penerimaan_arsip", nullable = false)
+	@Temporal(TemporalType.DATE)
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+	private Date tanggalPenerimaanArsip;
+	
+	@Column(name = "jam_penerimaan_arsip", nullable = false)
+	@Temporal(TemporalType.TIME)
+	@JsonFormat(pattern = "HH:mm", timezone = "GMT+8")
+	private Date jamPenerimaanArsip;
+	
+	@Column(name = "diterima_dari", nullable = false)
+	private String diterimaDari;
+	
+	@Column(name = "nomor_surat", nullable = false)
+	private String nomorSurat;
+	
+	@Column(name = "tanggal_surat", nullable = false)
+	@Temporal(TemporalType.DATE)
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+	private Date tanggalSurat;
+	
+	@Column(name = "perihal", nullable = false)
+	private String perihal;
+	
+	@Column(name = "lampiran")
+	private String lampiran;
+	
+	@Column(name = "kode_penyimpanan")
+	private String kodePenyimpanan;
+	
+	@Column(name = "keterangan")
+	private String keterangan;
+	
+	@Column(name = "url_file", columnDefinition="TEXT")
+	private String urlFile;
+	
+}
