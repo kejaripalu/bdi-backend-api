9f9896f1400a088d0b562373e08cc89dafc39561
Ucup Topekox <ucup.topekox@gmail.com>
2025-10-04 20:20:47 +0800

    update Register Kegiatan Intelijen

--- src/main/java/id/go/kejaripalu/bdi/service/RegisterKegiatanIntelijenService.java
+++ src/main/java/id/go/kejaripalu/bdi/service/RegisterKegiatanIntelijenService.java
@@ -2,10 +2,6 @@ package id.go.kejaripalu.bdi.service;
 
 import org.springframework.data.domain.Page;
 
-import id.go.kejaripalu.bdi.domain.RegisterKegiatanIntelijen;
-import id.go.kejaripalu.bdi.dto.RegisterKegiatanIntelijenRequest;
-import id.go.kejaripalu.bdi.dto.RegisterKegiatanIntelijenResponse;
-
 public interface RegisterKegiatanIntelijenService<T> extends CrudGenericService<T> {
 
 	Page<T> findAll(String startDate, String endDate, String stringBidangDirektorat, Integer pages, Integer sizes);
