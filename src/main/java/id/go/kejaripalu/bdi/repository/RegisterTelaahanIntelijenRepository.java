0cb22350f6a297130e21fefc517834f2ad574fd6
Ucup Topekox <ucup.topekox@gmail.com>
2025-10-14 22:20:14 +0800

    update & refactor Reg. Telaahan Intelijen

--- src/main/java/id/go/kejaripalu/bdi/repository/RegisterTelaahanIntelijenRepository.java
+++ src/main/java/id/go/kejaripalu/bdi/repository/RegisterTelaahanIntelijenRepository.java
@@ -3,6 +3,7 @@ package id.go.kejaripalu.bdi.repository;
 import java.util.Date;
 import java.util.Optional;
 
+import id.go.kejaripalu.bdi.dto.RegisterTelaahanIntelijenDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,7 +16,7 @@ public interface RegisterTelaahanIntelijenRepository extends JpaRepository<Regis
 	@Query("SELECT r FROM RegisterTelaahanIntelijen r WHERE r.deleted=false "
 			+ "AND r.tanggal BETWEEN :startDate AND :endDate "
 			+ "ORDER BY r.id DESC")
-	Page<RegisterTelaahanIntelijen> findAllLahin(Date startDate, Date endDate, Pageable pageable);
+	Page<RegisterTelaahanIntelijenDTO> findAllLahin(Date startDate, Date endDate, Pageable pageable);
 	
 	@Query("SELECT r FROM RegisterTelaahanIntelijen r WHERE r.deleted=false "
 			+ "AND (LOWER(r.nomor) LIKE LOWER(CONCAT('%', :value, '%')) "
@@ -23,13 +24,13 @@ public interface RegisterTelaahanIntelijenRepository extends JpaRepository<Regis
 			+ "OR LOWER(r.perihal) LIKE LOWER(CONCAT('%', :value, '%')) "
 			+ "OR LOWER(r.tindakLanjut) LIKE LOWER(CONCAT('%', :value, '%'))) "
 			+ "AND r.tanggal BETWEEN :startDate AND :endDate ORDER BY r.tanggal  DESC")
-	Page<RegisterTelaahanIntelijen> findBySearching(Date startDate, Date endDate, String value, Pageable pageable);
+	Page<RegisterTelaahanIntelijenDTO> findBySearching(Date startDate, Date endDate, String value, Pageable pageable);
 	
 	Optional<RegisterTelaahanIntelijen> findByIdsAndDeletedFalse(String ids);
 	
 	@Query("SELECT r FROM RegisterTelaahanIntelijen r WHERE r.deleted=false "
 			+ "AND r.tanggal BETWEEN :startDate AND :endDate "
 			+ "ORDER BY r.id DESC, r.tanggal  DESC")
-	Page<RegisterTelaahanIntelijen> findAllLahinToPrint(Date startDate, Date endDate, Pageable pageable);
+	Page<RegisterTelaahanIntelijenDTO> findAllLahinToPrint(Date startDate, Date endDate, Pageable pageable);
 	
 }
--- src/main/java/id/go/kejaripalu/bdi/service/RegisterTelaahanIntelijenService.java
+++ src/main/java/id/go/kejaripalu/bdi/service/RegisterTelaahanIntelijenService.java
@@ -1,23 +1,12 @@
 package id.go.kejaripalu.bdi.service;
 
+import id.go.kejaripalu.bdi.dto.RegisterTelaahanIntelijenDTO;
 import org.springframework.data.domain.Page;
 
-import id.go.kejaripalu.bdi.domain.RegisterTelaahanIntelijen;
-import id.go.kejaripalu.bdi.dto.RegisterTelaahanIntelijenRequest;
-import id.go.kejaripalu.bdi.dto.RegisterTelaahanIntelijenResponse;
-
-public interface RegisterTelaahanIntelijenService {
+public interface RegisterTelaahanIntelijenService extends CrudGenericService<RegisterTelaahanIntelijenDTO> {
 	
-	void create(RegisterTelaahanIntelijenRequest request);
+	Page<RegisterTelaahanIntelijenDTO> findAll(String start, String end, Integer pages, Integer sizes);
 	
-	void update(String ids, RegisterTelaahanIntelijenRequest request);
-	
-	Page<RegisterTelaahanIntelijen> findAll(String start, String end, Integer pages, Integer sizes);
-	
-	Page<RegisterTelaahanIntelijen> findBySearching(String start, String end, String value, Integer pages, Integer sizes);
-	
-	RegisterTelaahanIntelijenResponse findByIds(String ids); 
-	
-	void delete(String ids);
+	Page<RegisterTelaahanIntelijenDTO> findBySearching(String start, String end, String value, Integer pages, Integer sizes);
 
 }
